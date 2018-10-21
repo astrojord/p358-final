ed19135dd493476a64efbc6260631f3940dd74ed
astrojord <44324201+astrojord@users.noreply.github.com>
2018-10-21 13:30:23 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# read conditions in from 2lptic
+# run integrator.py and get all that good stuff
+# format output in the way we need it for analysis
+# graphs?? plt.plot(physics)

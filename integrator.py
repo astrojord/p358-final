6a64e1e7a4b2d672911043424ae829470db47db4
Margie <mbruff@mbruff-pc.localdomain>
2018-10-25 21:03:18 -0400

    added functions and descriptions

--- integrator.py
+++ integrator.py
@@ -1,4 +1,6 @@
 import numpy as np
+from BHA import Body
+
 ########################################################################################
 # global constants
 H = 68.0 # km/s/Mpc
@@ -6,12 +8,90 @@ G = 6.67e-20 # km^3/kg s^2
 cdmMass = 0.0 # eV
 wdmMass = 0.0 # eV
 ########################################################################################
-def integrate():
-  return physics
 
-integrate()
-# that's it we've solved it
+def grav_accelerate(bod, neighbor_tree, ndims):
+    """
+    calculates gravitational acceleration on one particle
+
+    inputs
+    -----------------
+    bod : Body
+      one individual particle
+    neighbor_tree : Tree
+      the tree structure containing neighbor CM and M data
+
+    outputs:
+    -----------------
+    accel : ndarray
+      ndims x 1 array of accelerations
+    """
+    return accel
+
+def get_dxdt():
+    """
+    calculates total acceleration from equation of motion (yall can do this for
+    one body at a time and put the loop in integrate, or just all the bodies)
+
+    inputs
+    -----------------
+    var : type
+      description
+    var : type
+      description
+
+    outputs
+    -----------------
+    var : type
+      description
+    """
+    return
+
+
+def leapfrog():
+    """
+    implement the time step equations from leapfrog to get new positions and
+    velocities for all particles
+
+    inputs
+    ----------------
+    var : type
+      description
+
+    outputs
+    ----------------
+    var : type
+      description
+    """
+    return
+
+
+# use this to see what a body object is
+#bo = Body((3,4,5), 60)
+#print(bo)
+
+
+
+
+
+
+
+
+
+
+
+
 ########################################################################################
+
+
+
+
+
+
+
+
+
+
+"""
 def gravPotential(x1,x2,dmType): 
   # x1 and x2 are the position vectors of 2 particles; dmType = 0 for WDM, = 1 for CDM
   if dmType == 0:
@@ -24,3 +104,4 @@ def gravPotential(x1,x2,dmType):
   
   r = np.sqrt(((x1[0]-x2[0])**2)+((x1[1]-x2[1])**2)+((x1[2]-x2[2])**2))
   return -1*G*(m**2)/r # this is a scalar -- does it need to be a vector and if so how do we get direction
+"""

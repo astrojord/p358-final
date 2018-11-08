f115b2602d7182fa18c1ebe215e1ed67b208f240
Jordan Sheely <44324201+astrojord@users.noreply.github.com>
2018-11-07 20:46:42 -0500

    Update integrator.py
    re-consolidated gravitational acceleration calculation back into one thing

--- integrator.py
+++ integrator.py
@@ -13,37 +13,6 @@ omegaDE = 0  # dark energy
 cdmMass = 0.0  # eV
 wdmMass = 0.0  # eV
 ########################################################################################
-def grav_accelerate(bod, ptcl_tree):
-    """
-    calculates gravitational acceleration on one particle
-
-    inputs
-    -----------------
-    bod : Body
-      one individual particle with ndims position
-    neighbor_tree : Tree
-      the tree structure containing neighbor CM and M data
-
-    outputs:
-    -----------------
-    accel : ndarray
-      ndims x 1 array of accelerations
-    """
-    assert type(bod) == Body, "bod input must be a Body object"
-    # get neighbor list
-    neighbor_list = ptcl_tree.neighbors(bod)
-
-    accel = np.zeros(3)
-    for neigh in neighbor_list:
-        posit = neigh[0]
-        mass = neigh[1]
-        dvect = bod.pos
-        d = posit - dvect
-        dmag2 = np.dot(d, d)
-        accel += G * mass / dmag2 ** 1.5 * d
-
-    return accel
-
 def get_dxdt(bod, tau, ptcl_tree):
     """
     calculates total acceleration from equation of motion (yall can do this for
@@ -64,7 +33,8 @@ def get_dxdt(bod, tau, ptcl_tree):
     assert type(bod) == Body, "bod input must be a Body object"
     # get neighbor list
     neighbor_list = ptcl_tree.neighbors(bod)
-
+    
+    # calculate the negative of the gravitational potential
     negPotential = np.zeros(3)
     for neigh in neighbor_list:
         posit = neigh[0]
@@ -74,7 +44,8 @@ def get_dxdt(bod, tau, ptcl_tree):
         for i in range(0,2):
             negPotential[i] = -1*(G*mass*(posit[i]-dvect[i]))/((np.dot(r,r))**(3/2))
 
-    expansion = np.zeros(3) # H(t)v(t)
+    # calculate the H(tau)v(tau) term
+    expansion = np.zeros(3)
     a = a(tau,0)
     # how to get v(t)!
 
@@ -129,17 +100,3 @@ def leapfrog():
 # bo = Body((3,4,5), 60)
 # print(bo)
 ########################################################################################
-"""
-def gravPotential(x1,x2,dmType): 
-  # x1 and x2 are the position vectors of 2 particles; dmType = 0 for WDM, = 1 for CDM
-  if dmType == 0:
-    m = wdmMass
-  else if dmType == 1:
-    m = cdmMass
-  else 
-    print("Invalid dmType parameter given. Must be 0 (WDM) or 1 (CDM).")
-    return NaN
-
-  r = np.sqrt(((x1[0]-x2[0])**2)+((x1[1]-x2[1])**2)+((x1[2]-x2[2])**2))
-  return -1*G*(m**2)/r # this is a scalar -- does it need to be a vector and if so how do we get direction
-"""

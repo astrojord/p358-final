c5fe1f5e0a2035c51c98c54f011ec438d6602c29
cmace98 <44324316+cmace98@users.noreply.github.com>
2018-11-27 19:26:19 -0500

    Update int_test.py
    changed some parameters probably. added a file save that is unnecessary

--- int_test.py
+++ int_test.py
@@ -9,19 +9,19 @@ N = 3 #number of physical dimensions (plotting set for 3)
 nP = 1000 #number of particles
 bodz = []
 for i in range(nP): #generate some random particles
-    pos = 0.5-np.random.rand(N)
+    pos = (0.5-np.random.rand(N))*10**4
     print(pos)
-    vel = (0.5-np.random.rand(N))*0.05
-    bod = Body(pos=pos,mass=1,vel=vel,acc=np.zeros(N))
+    vel = (0.5-np.random.rand(N))*0
+    bod = Body(pos=pos,mass=1e9,vel=vel,acc=np.zeros(N))
     print(bod)
     bodz.append(bod)
 print(bodz)
 
 ti = 0 #initial time
-tf = 30 #final time
-h  = 1 #timestep
-l = 10000000 #box size, real fucking big rn because things are flying all over the place
-nSave = 30 #number of steps between each save
+tf = 9000 #final time
+h  = 20 #timestep
+l = 1e7 #box size, real fuckin big rn because things are flying all over the place
+nSave = 10 #number of steps between each save
 
 bodlist = integrate(bodz,ti,tf,h,N,l,nSave)
 print(bodz)
@@ -41,6 +41,8 @@ fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 ax.plot(pos1x,pos1y,pos1z,'b.')
 plt.show()
+
+np.savetxt('dmpos.txt',pos1)
 
 #plt.plot(pos1x,pos1y,'b.')
 #plt.show()

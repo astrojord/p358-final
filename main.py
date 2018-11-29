74d3d7de6468bf4a2ce225943dcbde5c044495ee
Margie <mbruff@unc.edu>
2018-11-29 11:50:36 -0500

    initial halos!

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 import numpy as np
+import make_halos as init #NFW_dist(r, rs), cusp_dist(r,rs), NFW_vel(r, rs, M, Rvir), cusp_vel(r,rs,M,Rvir), get_halos(fMH, fDENS, fVELS, rs, delta, npart, M_type, Rv_typ, bigrat, axis)
+import methast as mh
+import argparse
 
-# read conditions in from 2lptic
+# parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('profile', type=str,
+                    help='string of the profile type: "NFW" or "cusp"')
+parser.add_argument('npart', type=int,
+                    help='target total number of particles; note: this might change by +/-1')
+parser.add_argument('M', type=float,
+                    help='typical galactic dark matter halo mass')
+parser.add_argument('R', type=float,
+                    help='typical galactic dark matter halo virial radius')
+parser.add_argument('rs', type=float,
+                    help='scale radius for dark matter halo profiles')
+parser.add_argument('delta', type=float,
+                    help='delta value to use in metropolis hastings')
+parser.add_argument('bigrat', type=float,
+                    help='ratio of npart in larger halo (ex. 0.5 for equal size halos); note: must be less than 1')
+parser.add_argument('sep', type=float,
+                    help='separation between halos')
+parser.add_argument('axis', type=bool,
+                    help='true for sep on axis in plane of rotation, false for sep on axis perpendicular to rotation')
+
+args = parser.parse_args()
+
+#initialize the problem
+if args.profile == 'NFW':
+    fDENS = init.NFW_dist
+    fVELS = init.NFW_vel
+elif args.profile == 'cusp':
+    fDENS = init.cusp_dist
+    fVELS = init.cusp_vel
+
+# get inital conditios
+halos_init = init.get_halos(mh.methast, fDENS, fVELS, args.rs, args.delta, args.npart, args.M, args.R, args.bigrat, args.sep, args.axis)
+print(halos_init)
+
 # run integrator.py and get all that good stuff
 # format output in the way we need it for analysis
 # graphs?? plt.plot(physics)
--- make_halos.py
+++ make_halos.py
@@ -5,18 +5,18 @@ G = 4.5245e-12 #kpc^3/(Msun*My^2)
 
 def NFW_dist(r, rs):
     dens = (r/rs)**(-1.)*(1+r/rs)**(-2.)
-
+    return dens
 def cusp_dist(r, rs):
     dens = r**(-1.)*(r+rs)**(-3.)
-
+    return dens
 def NFW_vel(r, rs, M, Rvir):
     v = np.sqrt(3.*G*M*rs**2. / Rvir**3. / (Rvir/rs) / (1.+Rvir/rs)**2. * (np.log(1.+r/rs) / (r/rs) - 1./(1.+r/rs)))
-
+    return v
 def cusp_vel(r, rs, M, Rvir):
     v = np.sqrt(G*M/12./a * (12.*r*(r+a)**3./a**4. * np.log((r+a)/r) - r/(r+a) * (25.+52.*r/a+42.*(r/a)**2.+12.*r/a)))
-    
+    return v
 
-def get_halos(fMH, fDENS, fVELS, rs, delta, npart, M_typ, Rv_typ, bigrat, sep):
+def get_halos(fMH, fDENS, fVELS, rs, delta, npart, M_typ, Rv_typ, bigrat, sep, axis):
     """
     inputs:
         fMH : function
@@ -39,6 +39,9 @@ def get_halos(fMH, fDENS, fVELS, rs, delta, npart, M_typ, Rv_typ, bigrat, sep):
             must be less than 1, ratio of total particles in larger halo (ratio of mass to total mass where Mtotal = 2*Mtyp)
         sep : float
             separation between centers of halos
+        axis : bool
+            true for separation on axis in plane of rotation, false for separation on axis perpendicular to plane of rotation
+
     outputs:
         init_halos : list
             list of initial body objects for both halos
@@ -53,38 +56,41 @@ def get_halos(fMH, fDENS, fVELS, rs, delta, npart, M_typ, Rv_typ, bigrat, sep):
     Rvir2 = (M2/M_typ)**(1./3.) * Rv_typ
 
     #get r values from methast for each halo
-    r10 = np.zeros(int(npart1))
-    r20 = np.zeros(int(npart2))
-    r1 = methast(fDENS,rs,int(npart1),r10,delta)
-    r2 = methast(fDENS,rs,int(npart2),r20,delta)
+    r10 = 1.
+    r20 = 1.
+    r1 = fMH(fDENS,rs,int(npart1),r10,delta)
+    r2 = fMH(fDENS,rs,int(npart2),r20,delta)
 
     #calcualte velocities
     vel1 = fVELS(r1, rs, M1, Rvir1)
     vel2 = fVELS(r2, rs, M2, Rvir2)
 
     #uniformly select phi and theta for positions
-    phi1 = random.random.uniform(0,2.*np.pi, len(r1))
-    theta1 = random.random.uniform(0,np.pi, len(r1))
-    phi2 = random.random.uniform(0,2.*np.pi, len(r2))
-    theta2 = random.random.uniform(0,np.pi, len(r2))
+    phi1 = np.random.uniform(0,2.*np.pi, len(r1))
+    theta1 = np.random.uniform(0,np.pi, len(r1))
+    phi2 = np.random.uniform(0,2.*np.pi, len(r2))
+    theta2 = np.random.uniform(0,np.pi, len(r2))
 
     #convert to cartesian
     pos1 = np.array([r1*np.sin(theta1)*np.cos(phi1), r1*np.sin(theta1)*np.sin(phi1), r1*np.cos(theta1)])
     pos2 = np.array([r2*np.sin(theta2)*np.cos(phi2), r2*np.sin(theta2)*np.sin(phi2), r2*np.cos(theta2)])
 
-    vel1 = np.array([-1.*np.sin(phi1), np.cos(phi1), 0])
-    vel2 = np.array([-1.*np.sin(phi2), np.cos(phi2), 0])
+    vel1 = np.array([-1.*np.sin(phi1), np.cos(phi1), np.zeros(npart1)])
+    print(np.shape(vel1))
+    vel2 = np.array([-1.*np.sin(phi2), np.cos(phi2), np.zeros(npart2)])
 
     #shift one halo sep/2 to the left and the other sep/2 to the right
     if axis == True:
-        pos1[0] += sep/2.
-        pos2[0] -= sep/2.
+        pos1[0,:] += sep/2.
+        pos2[0,:] -= sep/2.
     elif axis == False:
-        pos1[2] += sep/2.
-        pos2[2] -= sep/2.
-        
+        pos1[2,:] += sep/2.
+        pos2[2,:] -= sep/2.
+
     #make a list of body objects to output
     for i in range(npart1):
-        bod = Body(pos=pos1[:,i],mass=mpp,vel=vel1[:,i],acc=np.zeros(N),halonum = 1) #check type of halonum
+        bod = Body(pos=pos1[:,i],mass=mpp,vel=vel1[:,i],acc=np.zeros(3),halonum = 1) #check type of halonum
     for i in range(npart2):
-        bod = Body(pos=pos2[:,i],mass=mpp,vel=vel2[:,i],acc=np.zeros(N),halonum = 2) #check type of halonum
+        bod = Body(pos=pos2[:,i],mass=mpp,vel=vel2[:,i],acc=np.zeros(3),halonum = 2) #check type of halonum
+
+print(cusp_dist(np.ones(5), 10))
